9234a77db3d280eec95d520afed2b19931123f7b
Kaieida <63949971+Kaieida@users.noreply.github.com>
2020-07-10 21:33:55 +0300

    Update

---
+++ Assets/Scripts/BossAbilities.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossAbilities : MonoBehaviour
+{
+    GameObject player;
+    [SerializeField]
+    GameObject fireAbility;
+    void Start()
+    {
+        player = GameObject.Find("Player(Clone)");
+        StartCoroutine(FireAbility());
+    }
+    IEnumerator FireAbility()
+    {
+        while (true)
+        {
+            float distance = Vector3.Distance(player.transform.position, transform.position);
+            if (distance <= 20)
+            {
+                GameObject firePillar = Instantiate(fireAbility, player.transform.position, Quaternion.Euler(-90,0,0));
+                Destroy(firePillar, 2f);
+                //Debug.Log("Casting ability");
+                yield return new WaitForSeconds(10f);
+            }
+            else
+            {
+                yield return new WaitForSeconds(0.5f);
+            }
+        }
+    }
+    
+}
